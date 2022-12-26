79cea30c2a58ba912ee95088343212443657717a
ZafrinSyeda <syeda.zafrin@gmail.com>
2022-12-26 16:28:25 +0000

    added edit and delete options for locations

--- superherosighting/src/main/java/com/company/superherosighting/controllers/LocationController.java
+++ superherosighting/src/main/java/com/company/superherosighting/controllers/LocationController.java
@@ -3,11 +3,15 @@ package com.company.superherosighting.controllers;
 import com.company.superherosighting.dao.LocationDaoDB;
 import com.company.superherosighting.dao.SuperherovillainDaoDB;
 import com.company.superherosighting.entities.Location;
+import com.company.superherosighting.entities.Organisation;
 import com.company.superherosighting.entities.Sighting;
+import com.company.superherosighting.entities.Superherovillain;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,6 +28,46 @@ public class LocationController {
         List<Location> locations = locationDao.getAllLocations();
         model.addAttribute("locations", locations);
         return "locations";
+    }
+
+    @GetMapping("deleteLocation")
+    public String deleteLocation(Integer id) {
+        locationDao.deleteLocationById(id);
+        return "redirect:/locations";
+    }
+
+    @GetMapping("editLocation")
+    public String editLocation(Integer id, Model model) {
+        Location loc = locationDao.getLocationById(id);
+        model.addAttribute("loc", loc);
+        return "editLocation";
+    }
+
+    @PostMapping("editLocation")
+    public String postEditLocation(Location loc, HttpServletRequest request) {
+        loc.setName(request.getParameter("locationName"));
+        loc.setDescription(request.getParameter("locationDescription"));
+        loc.setAddress(request.getParameter("address"));
+        loc.setCity(request.getParameter("city"));
+        loc.setCountry(request.getParameter("country"));
+        loc.setPostcode(request.getParameter("postcode"));
+        if(request.getParameter("longitude") != null) {
+            try {
+                loc.setLongitude(Double.parseDouble(request.getParameter("longitude")));
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        if (request.getParameter("latitude") != null) {
+            try {
+                loc.setLatitude(Double.parseDouble(request.getParameter("latitude")));
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        locationDao.editLocation(loc);
+
+        return "redirect:/locations";
     }
 }
 
--- superherosighting/src/main/java/com/company/superherosighting/controllers/SuperherovillainController.java
+++ superherosighting/src/main/java/com/company/superherosighting/controllers/SuperherovillainController.java
@@ -1,18 +1,33 @@
 package com.company.superherosighting.controllers;
 
+import com.company.superherosighting.dao.LocationDaoDB;
+import com.company.superherosighting.dao.OrganisationDaoDB;
 import com.company.superherosighting.dao.SuperherovillainDaoDB;
+import com.company.superherosighting.entities.Location;
+import com.company.superherosighting.entities.Organisation;
+import com.company.superherosighting.entities.Sighting;
 import com.company.superherosighting.entities.Superherovillain;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class SuperherovillainController {
     @Autowired
     SuperherovillainDaoDB superDao;
+
+    @Autowired
+    LocationDaoDB locationDao;
+
+    @Autowired
+    OrganisationDaoDB orgDao;
 
     @GetMapping("superheroes")
     public String displaySightings(Model model) {
@@ -28,4 +43,36 @@ public class SuperherovillainController {
         superDao.deleteSuperherovillainById(id);
         return "redirect:/superheroes";
     }
+
+    @GetMapping("editSuperhero")
+    public String editSuperherovillain(Integer id, Model model) {
+        Superherovillain shv = superDao.getSuperherovillainById(id);
+        List<Organisation> organisations = orgDao.getAllOrganisations();
+        model.addAttribute("shv", shv);
+        model.addAttribute("organisations", organisations);
+        return "editSuperhero";
+    }
+
+    @PostMapping("editSuperhero")
+    public String postEditSuperherovillain(Superherovillain shv, HttpServletRequest request) {
+        shv.setName(request.getParameter("heroName"));
+        shv.setDescription(request.getParameter("heroDesc"));
+        shv.setSuperpower(request.getParameter("superpower"));
+        String orgId = request.getParameter("organisationId");
+        if (orgId != null) {
+            shv.setOrganisation(orgDao.getOrganisationById(Integer.parseInt(orgId)));
+        } else {
+            shv.setOrganisation(null);
+        }
+        String isHeroStr = request.getParameter("isHero");
+        if (isHeroStr == null) {
+            shv.setHero(false);
+        } else {
+            shv.setHero(true);
+        }
+        superDao.editSuperherovillain(shv);
+
+        return "redirect:/superheroes";
+    }
+
 }
